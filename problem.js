0360cc8b0766c9b7f5f5598a02ed0bffab9f9522
Md. Sidur Rahaman Rupom <111562438+iamrupom07@users.noreply.github.com>
2026-01-20 12:25:55 +0600

    Add multiple utility functions for string and array operations

---
+++ problem.js
@@ -0,0 +1,130 @@
+//Problem 1: Reverse a String
+
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+//Problem 2: Count Vowels in a String
+
+function countVowels(str) {
+  let count = 0;
+  let vowels = "aeiouAEIOU";
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
+//Problem 3: Check for Palindrome
+
+function isPalindrome(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return str === reversed;
+}
+
+//Problem 4: Find the Maximum Number
+
+function findMax(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+//Problem 5: Remove Duplicates from an Array
+
+function removeDuplicates(arr) {
+  let unique = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!unique.includes(arr[i])) {
+      unique.push(arr[i]);
+    }
+  }
+  return unique;
+}
+
+//Problem 6: Sum of All Numbers in an Array
+
+function sumArray(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+//Problem 7: Find Even Numbers in an Array
+
+function findEvenNumbers(arr) {
+  let evens = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evens.push(arr[i]);
+    }
+  }
+  return evens;
+}
+
+//Problem 8: Capitalize First Letter of Each Word
+
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  let result = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word =
+      words[i][0].toUpperCase() + words[i].slice(1);
+    result.push(word);
+  }
+  return result.join(" ");
+}
+
+//Problem 9: Find the Factorial of a Number
+
+function factorial(num) {
+  let result = 1;
+
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+//Problem 10: PingPong Challenge
+
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+
+
+
